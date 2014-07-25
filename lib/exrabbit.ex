0cd6eaeea4568edaecffdc7710c06c36f0a86879
Alexei Sholik <alcosholik@gmail.com>
2014-07-25 14:36:38 +0300

    Put extracted records in their own module

--- lib/exrabbit.ex
+++ lib/exrabbit.ex
@@ -1,47 +1,5 @@
-require Record
-
-defmodule Exrabbit.Framing do
-  Record.defrecord :pbasic, :'P_basic', Record.extract(:'P_basic', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-end
-
-defmodule Exrabbit.Defs do
-  require Exrabbit.Framing
-
-  Record.defrecord :amqp_params_network, Record.extract(:amqp_params_network, from_lib: "amqp_client/include/amqp_client.hrl")
-  Record.defrecord :queue_declare, :'queue.declare', Record.extract(:'queue.declare', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :queue_declare_ok, :'queue.declare_ok', Record.extract(:'queue.declare_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :queue_bind, :'queue.bind', Record.extract(:'queue.bind', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :queue_bind_ok, :'queue.bind_ok', Record.extract(:'queue.bind_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_get, :'basic.get', Record.extract(:'basic.get', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_get_ok, :'basic.get_ok', Record.extract(:'basic.get_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_get_empty, :'basic.get_empty', Record.extract(:'basic.get_empty', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_ack, :'basic.ack', Record.extract(:'basic.ack', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_consume, :'basic.consume', Record.extract(:'basic.consume', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_consume_ok, :'basic.consume_ok', Record.extract(:'basic.consume_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_publish, :'basic.publish', Record.extract(:'basic.publish', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_cancel, :'basic.cancel', Record.extract(:'basic.cancel', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_cancel_ok, :'basic.cancel_ok', Record.extract(:'basic.cancel_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_deliver, :'basic.deliver', Record.extract(:'basic.deliver', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_declare, :'exchange.declare', Record.extract(:'exchange.declare', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_declare_ok, :'exchange.declare_ok', Record.extract(:'exchange.declare_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_delete, :'exchange.delete', Record.extract(:'exchange.delete', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_delete_ok, :'exchange.delete_ok', Record.extract(:'exchange.delete_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_bind, :'exchange.bind', Record.extract(:'exchange.bind', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_bind_ok, :'exchange.bind_ok', Record.extract(:'exchange.bind_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_unbind, :'exchange.unbind', Record.extract(:'exchange.unbind', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :exchange_unbind_ok, :'exchange.unbind_ok', Record.extract(:'exchange.unbind_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_qos, :'basic.qos', Record.extract(:'basic.qos', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_qos_ok, :'basic.qos_ok', Record.extract(:'basic.qos_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :basic_nack, :'basic.nack', Record.extract(:'basic.nack', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :confirm_select, :'confirm.select', Record.extract(:'confirm.select', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :confirm_select_ok, :'confirm.select_ok', Record.extract(:'confirm.select_ok', from_lib: "rabbit_common/include/rabbit_framing.hrl")
-  Record.defrecord :amqp_msg, [props: Exrabbit.Framing.pbasic(), payload: ""]
-
-end
-
 defmodule Exrabbit.Utils do
-  import Exrabbit.Defs
-  import Exrabbit.Framing
+  use Exrabbit.Defs
 
   defp get_host do
     Application.get_env(:exrabbit, :broker_host, "localhost") |> String.to_char_list
@@ -235,4 +193,3 @@ defmodule Exrabbit.Utils do
   end
 
 end
-
---
+++ lib/exrabbit/records.ex
@@ -0,0 +1,54 @@
+defmodule Exrabbit.ExtractorUtils do
+  @moduledoc false
+
+  defmacro extract_all(names, lib) do
+    for name <- names do
+      elixir_name = name |> Atom.to_string |> String.replace(".", "_") |> String.to_atom
+      quote do
+        import Record
+        defrecord(unquote(elixir_name),
+                  unquote(name),
+                  Record.extract(unquote(name), from_lib: unquote(lib)))
+      end
+    end
+  end
+end
+
+import Record
+
+defmodule Exrabbit.Framing do
+  defrecord :pbasic, :'P_basic', Record.extract(:'P_basic', from_lib: "rabbit_common/include/rabbit_framing.hrl")
+end
+
+defmodule Exrabbit.Defs do
+  defmacro __using__(_) do
+    quote do
+      import Exrabbit.Framing
+      import Exrabbit.Defs
+    end
+  end
+
+  import Exrabbit.ExtractorUtils
+
+  [
+    :amqp_params_network,
+  ] |> extract_all("amqp_client/include/amqp_client.hrl")
+
+  [
+    :"queue.declare", :"queue.declare_ok", :"queue.bind", :"queue.bind_ok",
+
+    :"basic.get", :"basic.get_ok", :"basic.get_empty", :"basic.ack",
+    :"basic.consume", :"basic.consume_ok", :"basic.publish",
+    :"basic.cancel", :"basic.cancel_ok", :"basic.deliver",
+
+    :"exchange.declare", :"exchange.declare_ok", :"exchange.delete", :"exchange.delete_ok",
+    :"exchange.bind", :"exchange.bind_ok", :"exchange.unbind", :"exchange.unbind_ok",
+
+    :"basic.qos", :"basic.qos_ok", :"basic.nack",
+
+    :"confirm.select", :"confirm.select_ok",
+  ] |> extract_all("rabbit_common/include/rabbit_framing.hrl")
+
+  import Exrabbit.Framing
+  defrecord :amqp_msg, [props: pbasic(), payload: ""]
+end
